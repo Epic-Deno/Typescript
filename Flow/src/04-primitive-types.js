b55d5e803d678b2e72cba4f6a96dd09549bbdaa7
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-07-31 23:12:05 +0800

    原始数据类型

---
+++ Flow/src/04-primitive-types.js
@@ -0,0 +1,15 @@
+/**
+ * 原始数据类型
+ * @flow
+ */
+const a: string = 'foobar'
+
+const b: number = Infinity // NaN // 100
+
+const c: boolean = false // true
+
+const d: null = null
+
+const e: void = undefined
+
+const f: symbol = Symbol()
